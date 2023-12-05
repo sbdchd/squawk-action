e8c8bd93ef8897f91e5425d7ead791a092e81dd5
grantcooksey <suitcase24@gmail.com>
2023-12-04 23:18:11 -0500

    quotes work locally

---
+++ test/one-failure/a-unique-index.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX CONCURRENTLY dist_id_uniq ON distributors (dist_id);
---
+++ test/one-failure/text-field.sql
@@ -0,0 +1,4 @@
+CREATE TABLE "app_user" (
+    "id" serial NOT NULL PRIMARY KEY,
+    "email" varchar(100) NOT NULL
+);
